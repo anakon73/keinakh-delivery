27d33b602f9fb2f689535d68dc0405b48a844568
anakon73 <nazarkinah73@gmail.com>
2023-10-04 20:18:04 +0300

    feat(types): add order statuses

---
+++ src/types/models.ts
@@ -0,0 +1,14 @@
+export type orderStatus =
+  'customer_created' |
+  'customer_paid' |
+  'customer_cancelled' |
+  'kitchen_accepted' |
+  'kitchen_denied' |
+  'kitchen_refunded' |
+  'kitchen_preparing' |
+  'delivery_pending' |
+  'delivery_denied' |
+  'delivery_refunded' |
+  'delivery_picking' |
+  'delivery_delivering' |
+  'delivery_complete'
