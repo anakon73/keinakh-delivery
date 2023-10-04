dd03af643af6555a2b9a15c669108431d92968af
anakon73 <nazarkinah73@gmail.com>
2023-10-04 20:21:28 +0300

    feat(main): implement app file

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import Courier from './flows/Courier'
+import Customer from './flows/Customer'
+import Restraunt from './flows/Restraunt'
+
+class Application {
+  restraunt: Restraunt
+  customer: Customer
+  courier: Courier
+}
