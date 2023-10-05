db732d24666347f52de7bb44e086fc761edaad0a
anakon73 <nazarkinah73@gmail.com>
2023-10-05 16:43:31 +0300

    fix(restrauntMenuItem): correct types and remove balance field

--- src/entities/RestrauntMenuItem.ts
+++ src/entities/RestrauntMenuItem.ts
@@ -5,22 +5,19 @@ export default class RestrauntMenuItem {
   restrauntId: number
   name: string
   price: number
-  description: number
-  balance: number
+  description: string
 
   constructor(
     id: number,
     restrauntId: number,
     name: string,
     price: number,
-    description: number,
-    balance: number
+    description: string,
   ) {
     this.id = idGenerator()
     this.name = name
     this.restrauntId = restrauntId
     this.price = price
     this.description = description
-    this.balance = balance
   }
 }
