e7df057043ce6d0ad5cd99b0a449f16c773070f4
anakon73 <nazarkinah73@gmail.com>
2023-10-06 17:15:28 +0300

    feat(main): create method for create basic menu

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
+import Logger from './lib/Logger'
+
 import Courier from './flows/Courier'
 import Customer from './flows/Customer'
 import Restraunt from './flows/Restraunt'
+
+import menu from './db/menu'
+
+const logger = new Logger()
 
 class Application {
   restraunt: Restraunt
   customer: Customer
   courier: Courier
+
+  constructor() {
+    this.restraunt = new Restraunt('14th St New York')
+  }
+
+  createMenu() {
+    menu.forEach((item) => {
+      item.restrauntId = this.restraunt.id
+      this.restraunt.editMenu('add', item)
+    })
+  }
 }
+
+const app = new Application()
