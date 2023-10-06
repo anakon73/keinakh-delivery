0e84e5c7a9e1d7bfa742e125603afe110597c072
anakon73 <nazarkinah73@gmail.com>
2023-10-06 17:14:09 +0300

    feat(Menu): update methods to edit menu

--- src/entities/Menu.ts
+++ src/entities/Menu.ts
@@ -2,19 +2,23 @@ import RestrauntMenuItem from './RestrauntMenuItem'
 
 interface IMenu {
   addItem(item: RestrauntMenuItem): void
-  removeItem(itemName: string): void
+  removeItem(itemName: RestrauntMenuItem): void
   showMenu(): RestrauntMenuItem[]
 }
 
 export default class Menu implements IMenu {
-  #itemsList: RestrauntMenuItem[]
+  #itemsList: RestrauntMenuItem[] = []
 
   addItem(item: RestrauntMenuItem): void {
     this.#itemsList.push(item)
   }
 
-  removeItem(itemName: string) {
-    this.#itemsList.filter((item) => item.name !== itemName)
+  removeItem(item: RestrauntMenuItem) {
+    const index = this.#itemsList.indexOf(item)
+
+    if (index === -1) {
+      this.#itemsList.splice(index, 1)
+    }
   }
 
   showMenu(): RestrauntMenuItem[] {
