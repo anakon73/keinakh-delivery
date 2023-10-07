39e015f1751b45ea65385e624d6a165ef200194d
anakon73 <nazarkinah73@gmail.com>
2023-10-07 16:36:01 +0300

    feat(Order): implement new methods to add or remove items

--- src/entities/Order.ts
+++ src/entities/Order.ts
@@ -1,21 +1,27 @@
 import idGenerator from '../lib/idGenerator'
 
+import menu from '../db/menu'
+
 import { orderStatus } from '../types'
 
 import OrderItem from './OrderItem'
+import RestrauntMenuItem from './RestrauntMenuItem'
 
 interface IOrder {
-  addItem(item: OrderItem): void
+  addItem(item: RestrauntMenuItem, quantity: number): void
+  removeItem(item: OrderItem): void
+  showOrderItems(): OrderItem[]
 }
 
-export default class Order {
+export default class Order implements IOrder {
   id: number
   status: orderStatus
   customerId?: number
   restrauntId: number
   courierId?: number
   timestamp: Date
-  itemsList: OrderItem[] = []
+  itemsList: OrderItem[]
+  price: number
 
   constructor(
     customerId: number,
@@ -28,5 +34,27 @@ export default class Order {
     this.id = idGenerator()
     this.status = 'customer_created'
     this.timestamp = new Date()
+    this.itemsList = []
+    this.price = 0
+  }
+
+  addItem(item: RestrauntMenuItem, quantity: number): void {
+    const newOrderItem = new OrderItem(item, quantity)
+    this.price += newOrderItem.price * newOrderItem.quantity
+
+    this.itemsList.push(newOrderItem)
+  }
+
+  removeItem(item: OrderItem): void {
+    const index = this.itemsList.indexOf(item)
+
+    if (index !== -1) {
+      this.itemsList.splice(index, 1)
+    }
+  }
+
+  showOrderItems(): OrderItem[] {
+    return this.itemsList
   }
 }
+
